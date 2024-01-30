acc7f1d4f48776c8aae75afa9310c4ceb3e82130
a.strebizh <artem.strebizh@inappstory.com>
2024-01-30 11:40:27 +0300

    fix: return openUrl method (close via clsoeGame(openUrl) don`t work in iOS

--- src/sdkApi/openUrl.ts
+++ src/sdkApi/openUrl.ts
@@ -1,28 +1,28 @@
 import {iosMh, isAndroid, isIos, isWeb} from "../env";
 import {webSource} from "./web/Source";
 import {OpenUrlOptions} from "./openUrl.h";
-import {closeGameReader as sdkCloseGameReader} from './index';
+import {closeGameReader as sdkCloseGameReader} from "./index";
 
 export const openUrl = ({url, closeGameReader}: OpenUrlOptions) => {
 
-    if (closeGameReader) {
-        sdkCloseGameReader({openUrl: url});
-    } else {
-        if (isAndroid) {
-            if (window.Android.openUrl !== undefined) {
-                window.Android.openUrl(JSON.stringify({url}));
-            }
-        } else if (isIos) {
-            if (iosMh.openUrl !== undefined) {
-                iosMh.openUrl.postMessage(JSON.stringify({url}));
-            }
-        } else if (isWeb) {
-            if (webSource.sourceWindow && webSource.sourceWindowOrigin) {
-                webSource.sourceWindow.postMessage(
-                    ["openUrl", JSON.stringify({url})],
-                    webSource.sourceWindowOrigin
-                );
-            }
+    if (isAndroid) {
+        if (window.Android.openUrl !== undefined) {
+            window.Android.openUrl(JSON.stringify({url}));
         }
+    } else if (isIos) {
+        if (iosMh.openUrl !== undefined) {
+            iosMh.openUrl.postMessage(JSON.stringify({url}));
+        }
+    } else if (isWeb) {
+        if (webSource.sourceWindow && webSource.sourceWindowOrigin) {
+            webSource.sourceWindow.postMessage(
+                ["openUrl", JSON.stringify({url})],
+                webSource.sourceWindowOrigin
+            );
+        }
+    }
+
+    if (closeGameReader) {
+        sdkCloseGameReader();
     }
 };
