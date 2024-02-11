e7106541dbae76526e8dc32d053e2b1e64194829
a.strebizh <artem.strebizh@inappstory.com>
2024-02-11 23:40:42 +0300

    feat: add gameLoadingInfo and log gameInit process

--- src/sdkApi/initGame.ts
+++ src/sdkApi/initGame.ts
@@ -16,7 +16,17 @@ declare global {
         handleAudioFocusChange: (focusChange: number) => void;
         __debug?: boolean;
         _log: (text: string) => void;
+        _sendErrorLog: (payload: Record<string, any>) => void;
+        gameLoadingInfo: {
+            state: string;
+            description: string;
+        };
     }
+}
+
+window.gameLoadingInfo = {
+    state: "before gameReader API creation",
+    description: ""
 }
 
 type GameReaderInit = {
@@ -28,11 +38,43 @@ const gameReader: GameReaderInit = (function () {
     self._e = self._e || [];
     if (self._e) {
         for (let i = 0; i < self._e.length; i++) {
-            setTimeout(self._e[i], 0);
+            setTimeout((function (cb: () => void, i: number) {
+                return function () {
+                    try {
+                        window.gameLoadingInfo = {
+                            state: "before call gameReaderInit queue",
+                            description: "index: " + i
+                        }
+                        cb();
+                        window.gameLoadingInfo = {
+                            state: "after call gameReaderInit queue",
+                            description: "index: " + i
+                        }
+                    } catch (e) {
+                        window._sendErrorLog && window._sendErrorLog({src: "gameReaderInit queue", message: (e as Error).message, stack: (e as Error).stack});
+                        console.error(e);
+                    }
+                }
+            })(self._e[i], i));
         }
     }
     self.ready = function (cb) {
-        cb();
+        setTimeout(function () {
+            try {
+                window.gameLoadingInfo = {
+                    state: "before call gameReaderInit ready",
+                    description: ""
+                }
+                cb();
+                window.gameLoadingInfo = {
+                    state: "after call gameReaderInit ready",
+                    description: ""
+                }
+            } catch (e) {
+                window._sendErrorLog && window._sendErrorLog({src: "gameReaderInit ready", message: (e as Error).message, stack: (e as Error).stack});
+                console.error(e);
+            }
+        });
     };
     return self;
 })();
@@ -42,26 +84,41 @@ window.gameReader = gameReader;
 export const createInitGame = (initLocalData: () => Promise<void>, cb?: () => void) => {
 
     window.initGame = async function (config: GameLaunchConfig) {
-        if (!isObject(config)) {
-            console.error("Invalid gameConfig");
-            return;
+        try {
+            window.gameLoadingInfo = {
+                state: "before call initGame",
+                description: JSON.stringify(config)
+            }
+            if (!isObject(config)) {
+                console.error("Invalid gameConfig");
+                return;
+            }
+
+            // dto -> model
+            setGameLaunchConfig(config);
+
+            if (gameLaunchConfig.verbose) {
+                // @ts-ignore
+                window.__config = config;
+            }
+
+            if (gameLaunchConfig?.clientConfig?.nonce) {
+                createNonce(gameLaunchConfig?.clientConfig?.nonce);
+            }
+
+            await initLocalData();
+
+            cb && cb();
+
+            window.gameLoadingInfo = {
+                state: "after call initGame",
+                description: JSON.stringify(config)
+            }
+
+        } catch (e) {
+            window._sendErrorLog && window._sendErrorLog({src: "initGame", message: (e as Error).message, stack: (e as Error).stack});
+            console.error(e);
         }
-
-        // dto -> model
-        setGameLaunchConfig(config);
-
-        if (gameLaunchConfig.verbose) {
-            // @ts-ignore
-            window.__config = config;
-        }
-
-        if (gameLaunchConfig?.clientConfig?.nonce) {
-            createNonce(gameLaunchConfig?.clientConfig?.nonce);
-        }
-
-        await initLocalData();
-
-        cb && cb();
     };
 };
 
@@ -74,25 +131,46 @@ type GameLoadedSdkConfig = Partial<{
  * API method for remove loader screen from Reader
  */
 export const gameLoadedSdkCallback = (config?: Partial<GameLoadedSdkConfig>) => {
-    let showClose = config?.showClose;
-    if (showClose == null) {
-        showClose = false;
+    window.gameLoadingInfo = {
+        state: "before call gameLoadedSdkCallback",
+        description: ""
     }
-    let backGesture = config?.backGesture;
-    if (backGesture == null) {
-        backGesture = true;
-    }
-    if (isAndroid) {
-        if (window.Android.gameLoaded !== undefined) {
-            window.Android.gameLoaded(JSON.stringify({ showClose, backGesture }));
+    try {
+        let showClose = config?.showClose;
+        if (showClose == null) {
+            showClose = false;
         }
-    } else if (isIos) {
-        if (iosMh.gameLoaded !== undefined) {
-            iosMh.gameLoaded.postMessage(JSON.stringify({ showClose, backGesture: false }));
+        let backGesture = config?.backGesture;
+        if (backGesture == null) {
+            backGesture = true;
         }
-    } else if (isWeb) {
-        if (webSource.sourceWindow && webSource.sourceWindowOrigin) {
-            webSource.sourceWindow.postMessage(["gameLoaded", JSON.stringify({ showClose, backGesture })], webSource.sourceWindowOrigin);
+        if (isAndroid) {
+            if (window.Android.gameLoaded !== undefined) {
+                window.Android.gameLoaded(JSON.stringify({showClose, backGesture}));
+            }
+        } else if (isIos) {
+            if (iosMh.gameLoaded !== undefined) {
+                iosMh.gameLoaded.postMessage(JSON.stringify({showClose, backGesture: false}));
+            }
+        } else if (isWeb) {
+            if (webSource.sourceWindow && webSource.sourceWindowOrigin) {
+                webSource.sourceWindow.postMessage(["gameLoaded", JSON.stringify({
+                    showClose,
+                    backGesture
+                })], webSource.sourceWindowOrigin);
+            }
         }
+        window.gameLoadingInfo = {
+            state: "after call gameLoadedSdkCallback",
+            description: ""
+        }
+    } catch (e) {
+        window._sendErrorLog && window._sendErrorLog({src: "gameLoadedSdkCallback", message: (e as Error).message, stack: (e as Error).stack});
+        console.error(e);
     }
 };
+
+window.gameLoadingInfo = {
+    state: "gameReader API created",
+    description: ""
+}
