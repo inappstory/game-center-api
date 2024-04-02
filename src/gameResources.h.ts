fb4a9abae71891762d87ec91babfc81813065629
d.mokogon <workdenizz@gmail.com>
2024-04-02 17:39:59 +0700

    feat: move index.html into core but keep logging code & simplify GameResource type

--- src/gameResources.h.ts
+++ src/gameResources.h.ts
@@ -1,11 +1,6 @@
-const resourceUrlSymbol = Symbol.for("resourceUrl");
-type ResourceUrlSymbolType = typeof resourceUrlSymbol;
-type ResourceUrl<T extends string> = number & { [name in ResourceUrlSymbolType]: T };
-const resourceUrlBrand = <T extends string>(n: number) => n as ResourceUrl<T>;
-
-export type GameResourceItem = Record<string, ResourceUrl<"common">>;
+export type GameResourceItem = Record<string, string>;
 
 export type GameResources = {
-    fonts: GameResourceItem;
-    assets: GameResourceItem;
+    fonts: GameResourceItem,
+    assets: GameResourceItem
 };
